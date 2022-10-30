f5a72f3784711d0b985b4187e3d89db47581604a
Kindziolek <kingapietron@gmail.com>
2022-10-30 22:18:10 +0100

    Plant form - onSend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import styles from './App.module.css'
 import PlantListContainer from './containers/PlantListContainer'
 import PlantFormContainer from './containers/PlantFormContainer'
+import data from './plants.json'
+import { useState } from 'react'
+
 
 function App() {
+  const [plants, setPlants] = useState(data)
+  const onSend = (plant) => {
+    setPlants([...plants,plant])
+  }
   return (
     <div>
-      <PlantListContainer></PlantListContainer>
-      <PlantFormContainer></PlantFormContainer>
+      <PlantListContainer plants={plants}></PlantListContainer>
+      <PlantFormContainer onSend={onSend}></PlantFormContainer>
     </div>
   )
 }
--- src/containers/PlantListContainer.jsx
+++ src/containers/PlantListContainer.jsx
@@ -1,82 +1,11 @@
 import PlantListItem from '../components/PlantListItem';
 import styles from './PlantListContainer.module.css';
 
-const plants = [
-    {
-        name: "Filodendron Pink Princess",
-        dateOfPurchase: "10.10.2021",
-        placeOfPurchase: "Allegro",
-        price: "70 zł",
-        safeOfAnimals: "Nie",
-        photo: "https://a.allegroimg.com/s512/11726a/367c34d242bcb236da62dc4a6657/Philodendron-PINK-PRINCESS-filodendron-MALUCH",
-        notes: "xx",
-        substrate: "Hydroponika",
-        sizePot: "15cm",
-        sizePlant: "30cm",
-        newLeafDate: "25.10.2022",
-        place: "Jasne o rozproszonym świetle",
-        temperature: "16-25º C",
-        airHumidity: "60%",
-        watering: "Dopiero gdy przeschnie lub kiedy higrometr wskazuje 3",
-        wateringMetod: "Równomiernie na całej powierzchni",
-        phSubstrate: "5,5-6,0",
-        lighting: "Tak, ok. 6400K i minimum 500 lumenów",
-        reproduction: "Ukorzenianie odciętego szczytu rośliny, węzła lub łodygi, koniecznie koniecznie chociaż z jednym liściem, roślina musi też posiadać korzenie powietrzne. Ukorzenianie można przeprowadzić w wodzie z węglem",
-        fretilizer: "Nawóz do roślin zielonych",
-        fretilizerDate: "Data ostatniego nawożenia: 15.10.2022",
-        observations: "Preferuje luźne, dobrze przepuszczalne podłoże.",
-    },
-    {
-        name: "Filodendron Pink Princess",
-        dateOfPurchase: "10.10.2021",
-        placeOfPurchase: "Allegro",
-        price: "70 zł",
-        safeOfAnimals: "Nie",
-        photo: "https://a.allegroimg.com/s512/11726a/367c34d242bcb236da62dc4a6657/Philodendron-PINK-PRINCESS-filodendron-MALUCH",
-        notes: "xx",
-        substrate: "Hydroponika",
-        sizePot: "15cm",
-        sizePlant: "30cm",
-        newLeafDate: "25.10.2022",
-        place: "Jasne o rozproszonym świetle",
-        temperature: "16-25º C",
-        airHumidity: "60%",
-        watering: "Dopiero gdy przeschnie lub kiedy higrometr wskazuje 3",
-        wateringMetod: "Równomiernie na całej powierzchni",
-        phSubstrate: "5,5-6,0",
-        lighting: "Tak, ok. 6400K i minimum 500 lumenów",
-        reproduction: "Ukorzenianie odciętego szczytu rośliny, węzła lub łodygi, koniecznie koniecznie chociaż z jednym liściem, roślina musi też posiadać korzenie powietrzne. Ukorzenianie można przeprowadzić w wodzie z węglem",
-        fretilizer: "Nawóz do roślin zielonych",
-        fretilizerDate: "Data ostatniego nawożenia: 15.10.2022",
-        observations: "Preferuje luźne, dobrze przepuszczalne podłoże.",
-    },
-    {
-        name: "Filodendron Pink Princess",
-        dateOfPurchase: "10.10.2021",
-        placeOfPurchase: "Allegro",
-        price: "70 zł",
-        safeOfAnimals: "Nie",
-        photo: "https://a.allegroimg.com/s512/11726a/367c34d242bcb236da62dc4a6657/Philodendron-PINK-PRINCESS-filodendron-MALUCH",
-        notes: "xx",
-        substrate: "Hydroponika",
-        sizePot: "15cm",
-        sizePlant: "30cm",
-        newLeafDate: "25.10.2022",
-        place: "Jasne o rozproszonym świetle",
-        temperature: "16-25º C",
-        airHumidity: "60%",
-        watering: "Dopiero gdy przeschnie lub kiedy higrometr wskazuje 3",
-        wateringMetod: "Równomiernie na całej powierzchni",
-        phSubstrate: "5,5-6,0",
-        lighting: "Tak, ok. 6400K i minimum 500 lumenów",
-        reproduction: "Ukorzenianie odciętego szczytu rośliny, węzła lub łodygi, koniecznie koniecznie chociaż z jednym liściem, roślina musi też posiadać korzenie powietrzne. Ukorzenianie można przeprowadzić w wodzie z węglem",
-        fretilizer: "Nawóz do roślin zielonych",
-        fretilizerDate: "Data ostatniego nawożenia: 15.10.2022",
-        observations: "Preferuje luźne, dobrze przepuszczalne podłoże.",
-    },
-]
 
-function PlantListContainer() {
+
+function PlantListContainer({
+    plants
+}) {
     const listItems = plants.map((plant => {
     return (<li key={plant.name}><PlantListItem name={plant.name}></PlantListItem></li>
     )}))
