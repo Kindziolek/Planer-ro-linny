05e8b522300cd4973d189069e9e3fc5ab53bb313
Kindziolek <kingapietron@gmail.com>
2022-12-20 12:07:09 +0100

    back button fix, photo

--- src/components/PlantListItem.jsx
+++ src/components/PlantListItem.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import styles from './PlantListItem.module.scss';
 
+const defaultPhoto = 'https://img.freepik.com/vector-premium/coleccion-ilustraciones-plantas-interior_464940-31.jpg?w=2000'
+
 function PlantListItem({ id, name, photo }) {
   return (
     <div>
       <Link to={`/show/${id}`}>{name}</Link>
       <div className={styles.list_item}>
-        <img className={styles.photo} src={photo} />
+        <img className={styles.photo} src={photo || defaultPhoto} />
         <div className={styles.options}>
           <Link to={`/edit/${id}`}>Edycja</Link>
           <Link to={`/del/${id}`}>Usuń</Link>
